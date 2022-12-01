b1999e273544745f68b2d3dc5a2a3c301d410cd7
aisyaharsya <aisyaharsya1@gmail.com>
2022-12-01 11:19:22 +0700

    rincian transfer
    121313

--- resources/views/Cust/pembayaran1.blade.php
+++ resources/views/Cust/pembayaran1.blade.php
@@ -31,15 +31,43 @@
                     <div class="accordion accordion-flush accordion-margin" id="accordionFlushExample">
                         <div class="accordion-item mb-3">
                         <h2 class="accordion-header" id="flush-headingOne">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            Harga Total
+                            <button class="accordion-button collapsed " type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                <div class="row justify-content-between" >
+                                    <div class="col-4 me-5">
+                                        <p>Harga Total</p>
+                                    </div>
+                                    <div class="col-4 ms-5">
+                                        <h6><b>Rp979.116</b></h6>
+                                    </div>
+                                </div>
                             </button>
                         </h2>
                         <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                             <div class="accordion-body">
-                                <p>Bali Tour Package 2x</p>
-                                <p>Biaya Layanan</p>
-                                <p>Kode Unik</p>
+                                <div class="row justify-content-between">
+                                    <div class="col-4">
+                                        <p>Bali Tour Package 1x</p>
+                                    </div>
+                                    <div class="col-4">
+                                        <p>Rp960.000</p>
+                                    </div>
+                                </div>
+                                <div class="row justify-content-between">
+                                    <div class="col-4">
+                                        <p>Biaya Layanan</p>
+                                    </div>
+                                    <div class="col-4">
+                                        <p>Rp20.000</p>
+                                    </div>
+                                </div>
+                                <div class="row justify-content-between">
+                                    <div class="col-4">
+                                        <p>Kode Unik</p>
+                                    </div>
+                                    <div class="col-4">
+                                        <p>-Rp884</p>
+                                    </div>
+                                </div>
                             </div>
                             <a href="pembayarantf" class="btn btn-primary gap-2 col-10 mx-auto4">Bayar dengan transfer BCA</a>
                         </div>
--- resources/views/Cust/pembayarantf.blade.php
+++ resources/views/Cust/pembayarantf.blade.php
@@ -29,15 +29,36 @@
             <div class="col-lg-10" >
                 <div class="card1 card-margin">
                     <div class="card-header no-border">
-                        <h6 class="card-title"><b>Mohon transfer ke</b></h6>
+                        <h6 class="card-title"><i><i class="bi bi-wallet-fill"></i></i><b>Mohon transfer ke</b></h6>
                     </div>
                     <div class="card-body pt-0">
                         <div class="widget-49 widget-49-meeting-points">
-                                <p class="widget-49-meeting-item"><i><i class="bi bi-calendar4"></i></i><span>  Tanggal keberangkatan <b>10 Jan 2023 - 13 Jan 2023</b></span></p>
-                                <p class="widget-49-meeting-item" style="margin-top: 10px"><i><i class="bi bi-geo-alt-fill"></i></i><span>  Titik Kumpul <b>Holidayz Tour and Travel Office</b></span></p>
-                                <p class="widget-49-meeting-item"><span><b>   Jl. Raya Prapen No.23 A, Prapen, Kec. Tenggilis Mejoyo, Kota SBY, Jawa Timur 60239</b></span></p>
+                                <p class="widget-49-meeting-item"><span><b>PT. Holidayz Sejahtera</b></span></p>
+                                <div class="mb-4">
+                                    <a class="form-control2" id="formGroupExampleInput">087 888 2211</a>
+                                  </div>
+                                <p class="widget-49-meeting-item"><span><b>Jumlah Bayar</b></span></p>
+                                <div class="mb-4">
+                                    <a class="form-control2" id="formGroupExampleInput">Rp979.116</a>
+                                  </div>
                         </div>
                     </div>
+                    <div class="text-center alert alert-warning bg-warning border-0 alert-dismissible fade show" role="alert">
+                        PENTING! Mohon transfer sesuai jumlah yang tertera (termasuk 3 digit terakhir)
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                      </div>
+                </div>
+            </div>
+
+            <div class="col-lg-10" >
+                <div class="card1 card-margin">
+                    <div class="card-header no-border">
+                        <h5 class="card-title"><b>Sudah Menyelesaikan Transaksi?</b></h5>
+                    </div>
+                    <a class="form-control3" id="formGroupExampleInput">Setelah pembayaran Anda dikonfirmasi, kami akan mengirim bukti pembelian kepada anda</a>
+                    <a href="pembayarantf" class="btn btn-primary gap-2 col-10 mx-auto5"><b>Saya Sudah Bayar</b></a>
+                    </div>
+
                 </div>
             </div>
         </div>
